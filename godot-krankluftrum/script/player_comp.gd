05729abf42e71c7e03fd19f92272a9a366adec54
Barrjol <lokevestner@gmail.com>
2026-06-03 17:15:50 +0200

    Update player_comp.gd

--- godot-krankluftrum/script/player_comp.gd
+++ godot-krankluftrum/script/player_comp.gd
@@ -4,10 +4,18 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var input_dir = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("Upp"):
+		input_dir.y -= 1
+	if Input.is_action_pressed("Down"):
+		input_dir.y += 1
+	if Input.is_action_pressed("Left"):
+		input_dir.x -= 1
+	if Input.is_action_pressed("Right"):
+		input_dir.x += 1
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	input_dir = input_dir.normalized()
+	
+	var direction = (transform * Vector2(input_dir.x, input_dir.y)).normalized()
