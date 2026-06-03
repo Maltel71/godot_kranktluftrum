b86cce6b3a02293bceaf9463907dd054152e49e3
Barrjol <lokevestner@gmail.com>
2026-06-03 13:38:17 +0200

    added som codes

---
+++ godot-krankluftrum/Script/health_component.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name HealthComp
+
+@export var MAX_HEALTH := 100
+var Health : int
+
+signal Died
+signal Health_Changed
+func _ready():
+	Health = MAX_HEALTH
+	emit_signal("Health_Changed")
+	
+func Damage(Hit: HIT):
+	Health -= Hit.Hit_Damage
+	emit_signal("Health_Changed")
+	
+	
+	if Health <= 0:
+		emit_signal("Died")
