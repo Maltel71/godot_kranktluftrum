e86b0dd7dd1e5bd9201b4ca179a26d5c0f6f9b74
Barrjol <lokevestner@gmail.com>
2026-06-03 18:00:03 +0200

    can now fly

--- godot-krankluftrum/script/player_comp.gd
+++ godot-krankluftrum/script/player_comp.gd
@@ -1,21 +1,32 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 100
 
-var input_dir = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
+	var input_dir = Vector2.ZERO
 	if Input.is_action_pressed("Upp"):
 		input_dir.y -= 1
+
 	if Input.is_action_pressed("Down"):
 		input_dir.y += 1
+
 	if Input.is_action_pressed("Left"):
 		input_dir.x -= 1
+
 	if Input.is_action_pressed("Right"):
 		input_dir.x += 1
+
 	
 	input_dir = input_dir.normalized()
 	
-	var direction = (transform * Vector2(input_dir.x, input_dir.y)).normalized()
+	var direction = (Vector2(input_dir.x, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	move_and_slide()
--- godot-krankluftrum/script/shoot_comp.gd
+++ godot-krankluftrum/script/shoot_comp.gd
@@ -8,8 +8,10 @@ class_name ShootComp
 @export var BulletSpeed = 20
 @export var BulletSpread = 10
 
-var Bullet = bullet_scene.instantiate()
+#var Bullet = bullet_scene.instantiate()
+#Bullet.collision_layer = 1 << (Projectile_Layer - 1)
+#add_child(Bullet)
 
-Bullet.collision_layer = 1 << (Projectile_Layer - 1)
-
-add_child(Bullet)
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("Shoot")
+		FIRE
