4acd5da4a6350da5fe71f0f2f80a32c63ff31dfc
Barrjol <lokevestner@gmail.com>
2026-06-03 14:54:23 +0200

    added more scripts

--- godot-krankluftrum/Script/health_component.gd
+++ godot-krankluftrum/Script/health_component.gd
@@ -1,19 +1,19 @@
-extends Node
+extends Node2D
 class_name HealthComp
 
 @export var MAX_HEALTH := 100
-var Health : int
+var current_health : int
 
 signal Died
 signal Health_Changed
 func _ready():
-	Health = MAX_HEALTH
+	current_health = MAX_HEALTH
+	max(0, current_health - Damage)
 	emit_signal("Health_Changed")
-	
+
 func Damage(Hit: HIT):
-	Health -= Hit.Attack_Damage
+	current_health -= Hit.Attack_Damage
 	emit_signal("Health_Changed")
-	
-	
-	if Health <= 0:
+
+	if current_health <= 0:
 		emit_signal("Died")
--- godot-krankluftrum/Script/shoot_component.gd
+++ godot-krankluftrum/Script/shoot_component.gd
@@ -3,12 +3,7 @@ class_name ShootComp
 
 
 #@export var Projectile_Layer = 
-@export var Attack_Damage = 500000
+@export var Attack_Damage = 50
 @export var FireRate = 10
 @export var BulletSpeed = 20
 @export var BulletSpread = 10
-
-
-func _on_hit_body_entered():
-	if body.has_method("Damage"):
-		body.Damage(Attack_Damage)
