5b97b9634613589fc3f785bbe8cbac044dbaf8e2
Barrjol <lokevestner@gmail.com>
2026-06-04 12:10:54 +0200

    Update projectile_comp.gd

--- godot-krankluftrum/script/projectile_comp.gd
+++ godot-krankluftrum/script/projectile_comp.gd
@@ -4,7 +4,7 @@ class_name Projectile
 @export_category("Bullet")
 @export var Bullet_Speed: float = 300.0
 @export var Damage: int = 10
-@export var Bullet_Timer: int = 5
+@export var Bullet_Timer: int = 5.0
 
 
 @export_category("Missile")
@@ -13,11 +13,16 @@ class_name Projectile
 var direction: Vector2 = Vector2.ZERO
 var target: Node2D = null
 
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+func _on_area_entered(area):
+	queue_free()
+
 func _physics_process(delta: float) -> void:
 	if is_missile and target != null:
 		var steer = global_position.direction_to(target.global_position)
 		direction = direction.lerp(steer, 5.0 * delta).normalized()
 	Bullet_Timer -= delta
-	if Bullet_Timer == 0:
+	if Bullet_Timer <= 0:
 		queue_free()
 	position += direction * Bullet_Speed * delta
